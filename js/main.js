2668df90e71728b9c90429395b60d53ea8029316
easton-morris <eastonmorris9@gmail.com>
2021-07-20 20:14:14 -0700

    canvas now loads properly with text and image

--- js/main.js
+++ js/main.js
@@ -1,47 +1,45 @@
-let $canvas = document.getElementById('canvas');
+const $canvas = document.getElementById('canvas');
 const randImg = new Image();
 randImg.src = 'images\\grayexp.jpg';
-const imgH = randImg.height;
-const imgW = randImg.width;
-$canvas.setAttribute('height', imgH);
-$canvas.setAttribute('width', imgW);
-const quoteText = 'Resignation is what kills people. Once theyve rejected resignation, humans gain the privilege of making humanity their footpath. Alucard (Hellsing)';
-const canvasW = $canvas.width;
-const canvasH = $canvas.height;
+const quoteText = 'Resignation is what kills people. Once theyve rejected resignation, humans gain the privilege of making humanity their footpath.';
+const quoteAttr = 'Alucard (Hellsing)';
 const canvasCont = $canvas.getContext('2d');
 
 // function to load canvas and draw the quote//
 function canvasLoadImg() {
-  $canvas = $canvas.getContext('2d');
-  $canvas.drawImage(randImg, 0, 0, canvasW, canvasH);
+  canvasCont.drawImage(randImg, 0, 0, 1280, 720);
 }
 
 // function to take the quote and limit the number of char per line//
 function quoteWrap(quote) {
   const quoteWords = quoteText.split(' ');
   let currentLine = '';
-  const maxW = 400;
-  let fillH = 50;
+  const maxW = 1000;
+  let fillH = 720 / 3;
 
   for (let i = 0; i < quoteWords.length; i++) {
     const lineCheck = currentLine + quoteWords[i] + ' ';
     const checkWidth = canvasCont.measureText(lineCheck);
+    canvasCont.font = '4rem Playfair Display';
+    canvasCont.fillStyle = '#FFD700';
+    canvasCont.strokeStyle = 'black';
+    canvasCont.textAlign = 'center';
     if (checkWidth.width > maxW && i > 0) {
-      canvasCont.font = '4rem Playfair Display';
-      canvasCont.fillStyle = '#FFD700';
-      canvasCont.textAlign = 'center';
-      canvasCont.fillText(currentLine, canvasW / 2, fillH);
+      canvasCont.fillText(currentLine, 1280 / 2, fillH);
+      canvasCont.strokeText(currentLine, 1280 / 2, fillH);
       currentLine = quoteWords[i] + ' ';
-      fillH = fillH + 40;
+      fillH = fillH + 75;
     } else {
       currentLine = lineCheck;
     }
   }
-  canvasCont.font = '3rem Playfair Display';
-  canvasCont.fillStyle = '#FFD700';
-  canvasCont.textAlign = 'center';
-  canvasCont.fillText(currentLine, canvasW / 2, fillH);
+  canvasCont.fillText(currentLine, 1280 / 2, fillH);
+  canvasCont.strokeText(currentLine, 1280 / 2, fillH);
+  canvasCont.fillText(quoteAttr, 1280 / 2, fillH + 75);
+  canvasCont.strokeText(quoteAttr, 1280 / 2, fillH + 75);
 }
 
-randImg.onload = canvasLoadImg();
-quoteWrap(quoteText);
+randImg.addEventListener('load', function () {
+  canvasLoadImg();
+  quoteWrap(quoteText);
+});
