081934545ba428e671e305768724a5312bfd3bdc
easton-morris <eastonmorris9@gmail.com>
2021-07-22 21:19:31 -0700

    fixed the centering on the button and created the function for randomizing the image and quote

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 // DOM JS elements//
 const $canvas = document.getElementById('canvas');
+const $randomizeButton = document.querySelector('.randomize-button');
 
 // random image and quote variables//
 const randImg = new Image();
@@ -54,18 +55,18 @@ function quoteWrap(quote) {
   const quoteWords = quoteText.split(' ');
   let currentLine = '';
   const maxW = 1000;
-  let fillH = 720 / 3;
+  let fillH = 200;
 
   for (let i = 0; i < quoteWords.length; i++) {
     const lineCheck = currentLine + quoteWords[i] + ' ';
     const checkWidth = canvasCont.measureText(lineCheck);
-    canvasCont.font = '4rem Playfair Display';
+    canvasCont.font = 'bold 3rem Roboto';
     canvasCont.fillStyle = '#FFD700';
     canvasCont.strokeStyle = 'black';
     canvasCont.textAlign = 'center';
     if (checkWidth.width > maxW && i > 0) {
       canvasCont.fillText(currentLine, 1280 / 2, fillH);
-      /* canvasCont.strokeText(currentLine, 1280 / 2, fillH); */
+      canvasCont.strokeText(currentLine, 1280 / 2, fillH);
       currentLine = quoteWords[i] + ' ';
       fillH = fillH + 75; // space the lines of text out//
     } else {
@@ -73,9 +74,10 @@ function quoteWrap(quote) {
     }
   }
   canvasCont.fillText(currentLine, 1280 / 2, fillH);
-  /*  canvasCont.strokeText(currentLine, 1280 / 2, fillH); */
+  canvasCont.strokeText(currentLine, 1280 / 2, fillH);
+  canvasCont.font = 'bold 2rem Roboto';
   canvasCont.fillText(quoteAttr, 1280 / 2, fillH + 75); // load in the attribution separately//
-  /* canvasCont.strokeText(quoteAttr, 1280 / 2, fillH + 75); */
+  canvasCont.strokeText(quoteAttr, 1280 / 2, fillH + 75);
 }
 
 // populate new image and text once the image is ready//
@@ -84,5 +86,8 @@ randImg.addEventListener('load', function () {
   quoteWrap(quoteText);
 });
 
-getRandomQuote();
-getRandomImg();
+// event listener for randomizing on button click//
+$randomizeButton.addEventListener('click', function () {
+  getRandomQuote();
+  getRandomImg();
+});
